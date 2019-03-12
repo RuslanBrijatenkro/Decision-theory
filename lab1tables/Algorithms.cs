fa7f04fe925e6471c5bce0815d2173bde9134f2d
RuslanBrijatenkro <yahochuchalic228@gmail.com>
2019-03-13 00:15:09 +0200

    draw graphics

---
+++ lab1tables/Algorithms.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1tables
+{
+	class Algorithms
+	{
+		public static int[] mas1 = new int[] { 79, 95, 4, 37, 92, 95, 12, 52, 70, 14 };
+		public int[,] mas1q1q2 = new int[mas1.Length, 2];
+		public List<int[]> decisionPareto = new List<int[]>();
+		public List<int[]> decisionSlayter = new List<int[]>();
+		public void StartMethod()
+		{
+			CreateAlternative();
+			SlayterAlgorithm();
+		}
+		public void ParetoAlgorithm()
+		{
+			int i = 0, j = 1;
+			goto Two;
+		Two:
+			{
+				if (mas1q1q2[i, 0] >= mas1q1q2[j, 0] && mas1q1q2[i, 1] >= mas1q1q2[j, 1])
+					goto Three;
+				else
+					goto Five;
+			}
+		Three:
+			{
+				decisionPareto.RemoveAt(j);
+				decisionPareto.Insert(j, null);
+				goto Four;
+			}
+		Four:
+			{
+				if (j < decisionPareto.Count - 1)
+				{
+					j++;
+					goto Two;
+				}
+				else
+					goto Seven;
+			}
+		Five:
+			{
+				if (mas1q1q2[i, 0] <= mas1q1q2[j, 0] && mas1q1q2[i, 1] <= mas1q1q2[j, 1])
+					goto Six;
+				else
+					goto Four;
+
+			}
+		Six:
+			{
+				decisionPareto.RemoveAt(i);
+				decisionPareto.Insert(i, null);
+				goto Seven;
+			}
+		Seven:
+			{
+				if (i < decisionPareto.Count - 2)
+				{
+					i++;
+					j = i + 1;
+					goto Two;
+				}
+				else
+					goto End;
+			}
+		End:
+			{
+				foreach (int[] element in decisionPareto)
+				{
+					try
+					{
+						Console.Write(element[0] + " ");
+						Console.WriteLine(element[1]);
+					}
+					catch (Exception) { }
+				}
+			}
+		}
+		public void SlayterAlgorithm()
+		{
+			int i = 0, j = 1;
+			goto Two;
+		Two:
+			{
+				if (mas1q1q2[i, 0] > mas1q1q2[j, 0] && mas1q1q2[i, 1] > mas1q1q2[j, 1])
+					goto Three;
+				else
+					goto Five;
+			}
+		Three:
+			{
+				decisionSlayter.RemoveAt(j);
+				decisionSlayter.Insert(j, null);
+				goto Four;
+			}
+		Four:
+			{
+				if (j < decisionPareto.Count - 1)
+				{
+					j++;
+					goto Two;
+				}
+				else
+					goto Seven;
+			}
+		Five:
+			{
+				if (mas1q1q2[i, 0] < mas1q1q2[j, 0] && mas1q1q2[i, 1] < mas1q1q2[j, 1])
+					goto Six;
+				else
+					goto Four;
+
+			}
+		Six:
+			{
+				decisionSlayter.RemoveAt(i);
+				decisionSlayter.Insert(i, null);
+				goto Seven;
+			}
+		Seven:
+			{
+				if (i < decisionPareto.Count - 2)
+				{
+					i++;
+					j = i + 1;
+					goto Two;
+				}
+				else
+					goto End;
+			}
+		End:
+			{
+				foreach (int[] element in decisionSlayter)
+				{
+					try
+					{
+						Console.Write(element[0] + " ");
+						Console.WriteLine(element[1]);
+					}
+					catch (Exception) { }
+				}
+			}
+		}
+		public void CreateAlternative()
+		{
+			for (int i = 0; i < mas1.Length; i++)
+			{
+				mas1q1q2[i, 0] = mas1[i] / 10;
+				mas1q1q2[i, 1] = mas1[i] % 10;
+				decisionPareto.Add(new int[] { mas1q1q2[i, 0], mas1q1q2[i, 1] });
+				decisionSlayter.Add(new int[] { mas1q1q2[i, 0], mas1q1q2[i, 1] });
+			}
+		}
+	}
+}
---
+++ lab1tables/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1tables
+{
+	public partial class Form1 : Form
+	{
+		Algorithms algorithms = new Algorithms();
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		public void Start()
+		{
+			algorithms.StartMethod();
+			Graphics graphic = this.CreateGraphics();
+			Point[] points = new Point[algorithms.mas1q1q2.Length/2];
+			Pen pen = new Pen(Color.Black);
+			for(int i=0;i< algorithms.mas1q1q2.Length/2;i++)
+			{
+				points[i].X = algorithms.mas1q1q2[i, 0];
+				points[i].Y = algorithms.mas1q1q2[i, 1];
+				graphic.DrawRectangle(pen,points[i].X*100,points[i].Y*50,10,10);
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Start();
+		}
+	}
+}
