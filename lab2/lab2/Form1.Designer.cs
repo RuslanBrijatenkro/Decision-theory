08462bb76211110cbc499dcd38c2b0df404cb394
RuslanBrijatenkro <yahochuchalic228@gmail.com>
2019-03-13 21:00:16 +0200

    without tables

---
+++ lab2/lab2/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace lab2
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.button1 = new System.Windows.Forms.Button();
+			this.dataGridView2 = new System.Windows.Forms.DataGridView();
+			this.dataGridView3 = new System.Windows.Forms.DataGridView();
+			this.dataGridView4 = new System.Windows.Forms.DataGridView();
+			this.dataGridView5 = new System.Windows.Forms.DataGridView();
+			this.dataGridView6 = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(300, 200);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(451, 436);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// dataGridView2
+			// 
+			this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView2.Location = new System.Drawing.Point(337, 12);
+			this.dataGridView2.Name = "dataGridView2";
+			this.dataGridView2.Size = new System.Drawing.Size(305, 200);
+			this.dataGridView2.TabIndex = 2;
+			// 
+			// dataGridView3
+			// 
+			this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView3.Location = new System.Drawing.Point(667, 12);
+			this.dataGridView3.Name = "dataGridView3";
+			this.dataGridView3.Size = new System.Drawing.Size(305, 200);
+			this.dataGridView3.TabIndex = 3;
+			// 
+			// dataGridView4
+			// 
+			this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView4.Location = new System.Drawing.Point(12, 218);
+			this.dataGridView4.Name = "dataGridView4";
+			this.dataGridView4.Size = new System.Drawing.Size(300, 198);
+			this.dataGridView4.TabIndex = 4;
+			// 
+			// dataGridView5
+			// 
+			this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView5.Location = new System.Drawing.Point(338, 218);
+			this.dataGridView5.Name = "dataGridView5";
+			this.dataGridView5.Size = new System.Drawing.Size(304, 198);
+			this.dataGridView5.TabIndex = 5;
+			// 
+			// dataGridView6
+			// 
+			this.dataGridView6.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView6.Location = new System.Drawing.Point(667, 218);
+			this.dataGridView6.Name = "dataGridView6";
+			this.dataGridView6.Size = new System.Drawing.Size(304, 197);
+			this.dataGridView6.TabIndex = 6;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(984, 461);
+			this.Controls.Add(this.dataGridView6);
+			this.Controls.Add(this.dataGridView5);
+			this.Controls.Add(this.dataGridView4);
+			this.Controls.Add(this.dataGridView3);
+			this.Controls.Add(this.dataGridView2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.dataGridView1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.DataGridView dataGridView2;
+		private System.Windows.Forms.DataGridView dataGridView3;
+		private System.Windows.Forms.DataGridView dataGridView4;
+		private System.Windows.Forms.DataGridView dataGridView5;
+		private System.Windows.Forms.DataGridView dataGridView6;
+	}
+}
+
---
+++ lab2/lab2/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+	public partial class Form1 : Form
+	{
+		object[] results = new object[5];
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		void Start()
+		{
+			int[] goods1 = new int[] { 40, 29, 31, 17, 97, 71, 81, 75, 09, 27, 67, 56, 97, 53, 86, 65, 06, 83, 19, 24 };
+			int[] goods2 = new int[] { 28,71, 32, 29, 03, 19, 70, 68, 08, 15, 40, 49, 96, 23, 18, 45, 46, 51, 21, 55 };
+			int[] goods3 = new int[] { 79, 88, 64, 28, 41, 50, 93, 51, 34, 64, 24, 14, 87, 56, 43, 91, 27, 65, 59, 36 };
+			Algorithms algorithms = new Algorithms();
+			DataGridView dataGridView = dataGridView1;
+			dataGridView.Columns.Add("Data", "Data");
+			dataGridView.Columns.Add("NFA", "NFA");
+			dataGridView.Columns.Add("FFA", "FFA");
+			dataGridView.Columns.Add("WFA", "WFA");
+			dataGridView.Columns.Add("BFA", "BFA");
+			dataGridView.Columns["Data"].Width = 50;
+			dataGridView.Columns["NFA"].Width = 50;
+			dataGridView.Columns["FFA"].Width = 50;
+			dataGridView.Columns["WFA"].Width = 50;
+			dataGridView.Columns["BFA"].Width = 50;
+
+			results[0] = "1";
+			algorithms.NFA(goods1);
+			results[1] = algorithms.count;
+			algorithms.FFA(goods1);
+			results[2] = algorithms.count;
+			algorithms.WFA(goods1);
+			results[3] = algorithms.count;
+			algorithms.BFA(goods1);
+			results[4] = algorithms.count;
+			dataGridView.Rows.Add(results);
+
+			results[0] = "2";
+			algorithms.NFA(goods2);
+			results[1] = algorithms.count;
+			algorithms.FFA(goods2);
+			results[2] = algorithms.count;
+			algorithms.WFA(goods2);
+			results[3] = algorithms.count;
+			algorithms.BFA(goods2);
+			results[4] = algorithms.count;
+			dataGridView.Rows.Add(results);
+
+			results[0] = "3";
+			algorithms.NFA(goods3);
+			results[1] = algorithms.count;
+			algorithms.FFA(goods3);
+			results[2] = algorithms.count;
+			algorithms.WFA(goods3);
+			results[3] = algorithms.count;
+			algorithms.BFA(goods3);
+			results[4] = algorithms.count;
+			dataGridView.Rows.Add(results);
+		}
+
+		private void textbox_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Start();
+		}
+	}
+}
