5bd7a82bdf2d014071bafdb80bf47c8a7b747b57
RuslanBrijatenkro <yahochuchalic228@gmail.com>
2019-04-14 23:07:36 +0300

    new database

---
+++ lab4/lab4/Migrations/20190414005323_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace lab4.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GetVotes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    VotesCount = table.Column<int>(nullable: false),
+                    First = table.Column<string>(nullable: true),
+                    Second = table.Column<string>(nullable: true),
+                    Third = table.Column<string>(nullable: true),
+                    Fourth = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GetVotes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GetVotes");
+        }
+    }
+}
---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Linq;
+
+namespace lab4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			using (AppContext context = new AppContext())
+			{
+				var list = context.GetVotes.ToList();
+				foreach (var item in list)
+				{
+					Console.WriteLine($"{item.VotesCount} {item.First} {item.Second} {item.Third} {item.Fourth}");
+				}
+			}
+			Console.ReadKey();
+		}
+	}
+	class Votes
+	{
+		public int Id { get; set; }
+		public int VotesCount { get; set; }
+		public string First { get; set; }
+		public string Second { get; set; }
+		public string Third { get; set; }
+		public string Fourth { get; set; }
+	}
+	class AppContext: DbContext
+	{
+		public DbSet<Votes> GetVotes { get; set; }
+		
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=DESKTOP-OSOHBHJ; Database=lab4; Trusted_Connection=true;");
+		}
+	}
+}
